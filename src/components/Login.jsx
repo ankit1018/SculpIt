f0ae84554b7b3c64bb5bc9271f7e04a0a59b26ae
ankit1018 <112348837+ankit1018@users.noreply.github.com>
2023-09-20 16:41:35 +0530

    Initial commit

---
+++ src/components/Login.jsx
@@ -0,0 +1,38 @@
+import { useState } from "react";
+import Button from "@mui/material/Button";
+
+export default function Login(props) {
+  const [passcode, setPasscode] = useState("");
+  const [tryAgain, setTryAgain] = useState(false);
+
+  function wrong() {
+    setTryAgain(true);
+    setPasscode("");
+  }
+
+  function type(event) {
+    setPasscode(event.target.value);
+  }
+
+  return (
+    <div>
+      <div className="login_box">
+        <h1>Sculp It</h1>
+        <p className="pass">Enter password</p>
+        <p className="hint">
+          Hint: Its the firstname of the creator of this app.
+        </p>
+        <input onChange={type} type="password" placeholder="Password" />
+        <Button
+          style={{ height: "28px" }}
+          color="primary"
+          variant="contained"
+          onClick={passcode === "ankit" ? props.onclick : wrong}
+        >
+          Login
+        </Button>
+        {tryAgain && <p>Try Again</p>}
+      </div>
+    </div>
+  );
+}
